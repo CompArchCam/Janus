7b63f35af4f06d856798883b064abf54c0064b12
Xavier Chen <xavier.chen2000@gmail.com>
2022-10-19 10:49:52 +0100

    Changed testdir.sh to allow for testing for cfg mode as well; do not use this commit because more restructuring for the regression tests is on the way.

--- tests/polybench/x86-gcc/testdir.sh
+++ tests/polybench/x86-gcc/testdir.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/bash
+
+NC='\033[0m'
+RED='\033[31m'
+GREEN='\033[32m'
+
 function init() {
     for file in *.loop.select;
     do 
@@ -13,7 +18,7 @@ function init() {
     done
 }
 
-function run_test() {
+function parallelisation_test() {
     local aggregate=""
     for dir in *_test;
     do
@@ -26,16 +31,63 @@ function run_test() {
         if [ $? -ne 0 ];
         then
             aggregate="$aggregate"" ""$name"
-            echo "ERROR"
+            echo -e "$RED ERROR $NC"
         fi
         cd ..
     done
-    echo "$aggregate has failed"
+
+    if [[ $aggregate != "" ]]; then
+        echo -e "$RED $aggregate $NC has failed, check specific test outputs."
+    else
+        echo -e "$GREEN PASS $NC"
+    fi
+}
+
+function cfg_test() {
+    local loop_aggregate=""
+    local proc_aggregate=""
+    for dir in *_test;
+    do
+        name="${dir%_test}"
+        echo ">>>>>>>>>>$name<<<<<<<<<<"
+        cd $dir
+        ../../../../bin/analyze -cfg $name > "$name".cfg_out
+        diff "$name".loop.cfg "$name".master.loop.cfg
+        if [ $? -ne 0 ];
+        then
+            loop_aggregate="$loop_aggregate"" ""$name"
+            echo -e "$RED ERROR: loop cfg differs $NC"
+        else
+            echo -e "$GREEN PASS: loop cfg $NC"
+        fi
+
+        diff "$name".proc.cfg "$name".master.proc.cfg
+        if [ $? -ne 0 ];
+        then
+            proc_aggregate="$proc_aggregate"" ""$name"
+            echo -e "$RED ERROR: proc cfg differs $NC"
+        else
+            echo -e "$GREEN PASS: proc cfg $NC"
+        fi
+        cd ..
+    done
+    if [[ $loop_aggregate != "" ]]; then
+        echo -e "$RED FAILED: loop cfg differs in: $loop_aggregate $NC"
+    else
+        echo -e "$GREEN PASS: all loop cfg passed regression test $NC"
+    fi
+    if [[ $proc_aggregate != "" ]]; then
+        echo -e "$RED FAILED: loop cfg differs in: $proc_aggregate $NC"
+    else
+        echo -e "$GREEN PASS: all loop cfg passed regression test $NC"
+    fi
 }
 
 case $1'' in
     'init')
         init;;
-    'test')
-        run_test;;
+    'par_test')
+        parallelisation_test;;
+    'cfg_test')
+        cfg_test;;
 esac
